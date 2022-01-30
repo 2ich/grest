8bb6803ca0535ddff65e21b96463b04d7c80ec0e
Justeen-Weak <58949712+Justeen-Weak@users.noreply.github.com>
2022-01-30 12:23:37 +0300

    correct

--- sketch.js
+++ sketch.js
@@ -4,18 +4,18 @@ var blobs = []
 function setup() {
     createCanvas(600, 600)
     blob = new Blob(width/2, height/2, 64)
-    for (var i = 0; i < 10; i++) {
-        blobs[i] = new Blob(random(width), random(height), 16)
+    for (var i = 0; i < 60; i++) {
+        blobs[i] = new Blob(random(width * 2), random(height * 2), 16)
     }
 }
 
 function draw() {
-    background(20)
+    background(14)
 
     translate(width/2 - blob.pos.x, height/2 - blob.pos.y)
-    blob.update()
     blob.show()
 
+    blob.update()
     for (var i = 0; i < blobs.length; i++) {
         blobs[i].show()
     }
